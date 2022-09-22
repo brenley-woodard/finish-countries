4e4ba9d4ea507db0f66119ec63c64d60833df20a
brenley-woodard <brenleyew@gmail.com>
2022-09-21 17:26:53 -0700

    page displays counts

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ function displayNotifications() {
         // > Part D: Display a message with
         //      - how many items were returned in countries array
         //      - how many total matching countries were in the db
+        notificationDisplay.textContent = `Showing ${countries.length} of ${count} matching countries`;
     }
 }
 
